08f7894200fce33586a21cc7bd36d18a0880ecab
randeng <randeng@randengs-MacBook-Pro.local>
2023-12-09 04:04:42 +0800

    add @vercel/speed-insights

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,6 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
  
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
