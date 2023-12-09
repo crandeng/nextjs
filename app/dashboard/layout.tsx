0803ac850fea99f973df3a7adf5022e1cb2f8713
randeng <randeng@randengs-MacBook-Pro.local>
2023-12-10 01:56:20 +0800

    finished 13

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -10,8 +10,8 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       </div>
       <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
         {children}
-        <SpeedInsights />
-        <Analytics />
+        {/* <SpeedInsights />
+        <Analytics /> */}
       </div>
     </div>
   );
